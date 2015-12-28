f4f1bb8f1ecb74f05908d07effda3f9a8d977c84
peter <petrel.harp@gmail.com>
2015-12-28 00:33:34 -0800

    N fix

--- analyses/habitats/Xerospermophilus_mohavensis.R
+++ analyses/habitats/Xerospermophilus_mohavensis.R
@@ -29,13 +29,14 @@ if (FALSE) {
 
 habitat <- raster(orig.outfile)
 carrying.capacity <- 5000 * prod(res(habitat))/270^2  # complete guess
+habitable <- (!is.na(values(habitat)) & values(habitat)>0)
 pop <- population( 
                   habitat = habitat,
                   accessible = !is.na(values(habitat)),
-                  habitable = (!is.na(values(habitat)) & values(habitat)>0),
+                  habitable = habitable,
                   genotypes = c("aa","aA","AA"),
                   carrying.capacity = carrying.capacity,
-                  N = cbind( aa=rpois(length(carrying.capacity),carrying.capacity),
+                  N = cbind( aa=rpois(sum(habitable),carrying.capacity),
                              aA=0, 
                              AA=0 ),
                   description = "
--- analyses/habitats/Yucca_brevifolia.R
+++ analyses/habitats/Yucca_brevifolia.R
@@ -30,13 +30,14 @@ if (FALSE) {
 
 habitat <- raster(orig.outfile)
 carrying.capacity <- 120 * prod(res(habitat))/270^2  # estimated from google maps near desert queen mine
+habitable <- (!is.na(values(habitat)) & values(habitat)>0)
 pop <- population( 
                   habitat = habitat,
                   accessible = !is.na(values(habitat)),
-                  habitable = (!is.na(values(habitat)) & values(habitat)>0),
+                  habitable = habitable,
                   genotypes = c("aa","aA","AA"),
                   carrying.capacity = carrying.capacity,
-                  N = cbind( aa=rpois(length(carrying.capacity),carrying.capacity),
+                  N = cbind( aa=rpois(sum(habitable),carrying.capacity),
                              aA=0, 
                              AA=0 ),
                   description = "
